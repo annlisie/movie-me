9b69daa931c3fa66a5127242593908a32c3a4407
emmaluciano <emmaluciano@outlook.com>
2017-06-20 22:59:28 +0200

    logowanie i navbar

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,6 +4,8 @@ import { HttpModule, Http } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './authentication/registration/registration.component';
+import { LoginComponent } from "./authentication/login/login.component";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -13,7 +15,9 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent,
-        RegistrationComponent
+        RegistrationComponent,
+        LoginComponent,
+        NavbarComponent
       ],
       providers: [Http]
     }).compileComponents();
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,28 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './authentication/registration/registration.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './authentication/login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistrationComponent }
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
