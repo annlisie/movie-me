43938a46b9d9e0eeae625998dd3f94b7fc9d1608
emmaluciano <emmaluciano@outlook.com>
2017-06-20 23:07:34 +0200

    logowanie i navbar

---
+++ src/app/authentication/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {Headers, RequestOptions, Response, Http} from "@angular/http";
+
+@Component({
+  selector: 'app-authentication-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  model: any = {};
+
+  constructor(public http: Http) { }
+
+  ngOnInit() {
+  }
+
+
+  login(email: string, password: string) {
+
+    const url = 'https://localhost:8443/login';
+
+    const headers = new Headers ({ 'Content-Type': 'application/json' });
+    const options = new RequestOptions({ headers: headers});
+
+    const data = {
+      email: email,
+      password: password
+    };
+
+    return this.http.post( url, data, options )
+      .map((res: Response) => res.json())
+      .subscribe();
+  }
+
+}
