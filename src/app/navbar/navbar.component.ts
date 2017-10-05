cce3335def0cf7bcc723a5353165f9ffcff2fd83
emmaluciano <emmaluciano@outlook.com>
2017-10-05 19:54:27 +0200

    Logowanie i Rejestracja

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {EventManager} from './../authentication/event.manager';
+import {AuthenticationService} from './../authentication/authentication.service'
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
 
-  constructor() { }
+export class NavbarComponent implements OnInit {
+  public showNavBar: boolean;
+  public userEmail: string;
+  constructor(private eventManager: EventManager, private authenticationService: AuthenticationService) {
+    this.eventManager.showNavBarEmitter.subscribe((mode) => {
+      // mode will be null the first time it is created, so you need to igonore it when null
+      if (mode !== null) {
+        this.showNavBar = mode;
+        if (this.showNavBar) {
+          let user = JSON.parse(localStorage.getItem('currentUser'));
+          this.userEmail = user.email;
+          console.log(this.userEmail);
+        }
+      }
+    });
+  }
 
   ngOnInit() {
+  }
+
+  logout() {
+    console.log('Jestem tutaj!');
+    this.authenticationService.logout();
   }
 
 }
